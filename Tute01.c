79fc60ffdde35969a9ce0c8e1190fdc0d4704a3e
K.H.I.M WASANA <99290884+IT21197796@users.noreply.github.com>
2022-02-10 18:30:42 +0000

    completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int mark2,mark1,sum;
+  float avg;
+  printf("Input sub 1 mork : ");
+  scanf("%d", &mark1);
+  printf("Input sub 1 mork : ");
+  scanf("%d", &mark2);
+
+  //Calculations
+  sum = mark1 + mark2;
+  avg = sum/2.0;
+
+  printf("Average of the two marks : %.2f", avg);
+
   
   return 0;
 }
